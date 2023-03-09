98a54808149074e0cfada61b658896fc0b4d3570
sabiq7392 <sabiqmuhammad98@gmail.com>
2023-03-09 18:03:25 +0700

    try to recordd audio

--- app/src/main/java/com/example/canvasrecorderwithffmpeg/MainActivity.kt
+++ app/src/main/java/com/example/canvasrecorderwithffmpeg/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.canvasrecorderwithffmpeg
 
+import android.Manifest
 import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.view.SurfaceHolder
 import android.view.SurfaceView
-import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -20,115 +22,101 @@ private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
 
 class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
 
-    private lateinit var surfaceView: SurfaceView
-    private lateinit var surfaceHolder: SurfaceHolder
-    private lateinit var mediaPlayer: MediaPlayer
-    private lateinit var startRecording: Button
-    private lateinit var stopRecording: Button
-    private lateinit var replayVideo: Button
+    private var surfaceView: SurfaceView? = null
+    private var surfaceHolder: SurfaceHolder? = null
+    private var mediaPlayer: MediaPlayer? = null
+    private var buttonStartRecording: Button? = null
+    private var buttonStopRecording: Button? = null
+    private var buttonReplayVideo: Button? = null
     private var mediaRecorder: MediaRecorder? = null
-
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<String>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
-//            grantResults[0] == PackageManager.PERMISSION_GRANTED
-//        } else {
-//            false
-//        }
-//        if (!permissionToRecordAccepted) finish()
-//    }
-
+    private var permissionToRecordAccepted = false
+    private val PERMISSIONS_REQUEST_CAMERA = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startRecording = findViewById(R.id.start_recording)
-        stopRecording = findViewById(R.id.stop_recording)
-        stopRecording.setEnabled(false)
-        replayVideo = findViewById(R.id.replay_video)
+        buttonStartRecording = findViewById(R.id.start_recording)
+        buttonStopRecording = findViewById(R.id.stop_recording)
+        buttonStopRecording?.setEnabled(false)
+        buttonReplayVideo = findViewById(R.id.replay_video)
 
         surfaceView = findViewById(R.id.surface_view)
-        surfaceHolder = surfaceView.holder
-        surfaceHolder.addCallback(this)
+        surfaceHolder = surfaceView?.holder
+        surfaceHolder?.addCallback(this)
 
-        mediaRecorder = MediaRecorder()
 
+        buttonStartRecording?.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 1)
+            } else {
+                onStartRecording()
+                mediaPlayer?.setOnCompletionListener {
+                    onStopRecording()
+                }
+            }
+        }
+
+        buttonStopRecording?.setOnClickListener() {
+            onStopRecording()
+        }
     }
 
     override fun surfaceCreated(holder: SurfaceHolder) {
         mediaPlayer = MediaPlayer()
-        mediaPlayer.setDisplay(surfaceHolder)
+        mediaPlayer?.setDisplay(surfaceHolder)
 
         val videoUri = Uri.parse("https://kenangan.s3-ap-southeast-1.amazonaws.com/content-cache/video/43531af3-4e31-42dd-b40c-0e18901e892b.mp4")
-        mediaPlayer.setDataSource(this, videoUri)
-        mediaPlayer.setOnPreparedListener {
-            mediaPlayer.start()
-            replayVideo.setEnabled(false)
+        mediaPlayer?.setDataSource(this, videoUri)
+        mediaPlayer?.setOnPreparedListener {
             println("starting video....")
+            mediaPlayer?.start()
+            buttonReplayVideo?.setEnabled(false)
         }
-        mediaPlayer.prepareAsync()
-        mediaPlayer.setOnCompletionListener {
-            replayVideo.setEnabled(true)
-            println("stop  video...")
-        }
-
-
-        replayVideo.setOnClickListener() {
-            mediaPlayer.start()
-            replayVideo.setEnabled(false)
+        mediaPlayer?.prepareAsync()
+        buttonReplayVideo?.setOnClickListener() {
             println("replay video....")
+            mediaPlayer?.start()
+            buttonReplayVideo?.setEnabled(false)
         }
-
-        startRecording.setOnClickListener {
-            stopRecording.setEnabled(true)
-//            // Konfigurasi Media Recorder
-//            mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE)
-//            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-//            mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT)
-////            mediaRecorder.setVideoSize(surfaceView.width, surfaceView.height)
-//            mediaRecorder.setOutputFile(getOutputMediaFile().toString())
-//            mediaRecorder.setMaxDuration(5000)
-//            mediaRecorder.setPreviewDisplay(surfaceHolder.surface)
-//            mediaRecorder.prepare()
-
-            // Mulai merekam video
-//            mediaRecorder.start()
-            // Konfigurasi media recorder untuk merekam video
-//            mediaRecorder?.apply {
-//                setVideoSource(MediaRecorder.VideoSource.SURFACE)
-//                setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-//                setOutputFile(getOutputMediaFile().toString())
-//                setVideoEncodingBitRate(10000000)
-//                setVideoEncoder(MediaRecorder.VideoEncoder.H264) // Atur encoder video
-//                setVideoFrameRate(30)
-//                setMaxDuration((5000))
-//                setOnInfoListener { mr, what, extra ->
-//                    // Callback ketika media recorder memberikan informasi
-//                    when (what) {
-//                        MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED -> {
-//                            // Tindakan yang diambil ketika durasi maksimal tercapai
-//                            mediaRecorder?.stop()
-//                            mediaRecorder?.release()
-//                            stopRecording.setEnabled(false)
-//                            println("stop recording...")
-//                        }
-//                    }
-//                }
-//                prepare()
-//                start()
+//
+//        buttonStopRecording?.setOnClickListener() {
+//            mediaRecorder?.stop()
+//            mediaRecorder?.release()
+//        }
+//
+//
+//        buttonStartRecording?.setOnClickListener {
+//            println("start recording..." )
+//            if (ContextCompat.checkSelfPermission(
+//                    this,
+//                    Manifest.permission.CAMERA
+//                ) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+//                    this,
+//                    Manifest.permission.RECORD_AUDIO
+//                ) != PackageManager.PERMISSION_GRANTED
+//            ) {
+//                ActivityCompat.requestPermissions(
+//                    this,
+//                    arrayOf<String>(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO),
+//                    PERMISSIONS_REQUEST_CAMERA
+//                )
 //            }
-
-            mediaPlayer.seekTo(0)
-            startRecording.setEnabled(false)
-
-
-            println("start recording..." )
-        }
+////   else {
+//                buttonStartRecording?.setEnabled(false);
+//                buttonStopRecording?.setEnabled(true)
+//                mediaRecorder?.setVideoSource(MediaRecorder.VideoSource.SURFACE)
+//                mediaRecorder?.setPreviewDisplay(surfaceView?.getHolder()?.getSurface());
+//                mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+//                mediaRecorder?.setVideoFrameRate(60)
+////                mediaRecorder?.setVideoSize(surfaceView!!.getWidth(), surfaceView!!.getHeight())
+//                mediaRecorder?.setOutputFile(getOutputMediaFile())
+//                mediaRecorder?.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT)
+//                mediaRecorder?.setMaxDuration(5000)
+//                mediaRecorder?.prepare()
+//
+//                mediaRecorder?.start()
+////            }
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
@@ -136,28 +124,52 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
     }
 
     override fun surfaceDestroyed(holder: SurfaceHolder) {
+        println("surface destroyed...")
+
         // Stop and release MediaRecorder
         mediaRecorder?.stop()
         mediaRecorder?.release()
 
         // Stop and release MediaPlayer
-        mediaPlayer.stop()
-        mediaPlayer.release()
-        println("surface destroyed...")
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
     }
 
-    private fun getOutputMediaFile(): File? {
-        // Create a media file name
+    private fun onStopRecording() {
+        println("stop recording...")
+        mediaRecorder?.stop()
+        mediaRecorder?.release()
+        buttonStopRecording?.setEnabled(false)
+        buttonStartRecording?.setEnabled(true)
+    }
+
+    private fun onStartRecording() {
+        println("start recording...")
         val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-
-        println(Environment.DIRECTORY_DCIM)
-        println("get out put media file ...")
-
-        return File(
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM),
-            "VID_$timeStamp.mp4"
-        )
+        mediaRecorder = MediaRecorder()
+        mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        mediaRecorder?.setOutputFile("${Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)}/recording_${timeStamp}.3gp")
+        mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+        mediaRecorder?.prepare()
+        mediaRecorder?.start()
+        buttonStopRecording?.setEnabled(true)
+        buttonStartRecording?.setEnabled(false)
     }
+//    private fun getOutputMediaFile(): File? {
+//        println("get out put media file ...")
+//
+//        // Create a media file name
+//        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+//
+//        println(Environment.DIRECTORY_DCIM)
+//
+//        return File(
+//            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM),
+//            "VID_$timeStamp.mp4"
+////            "VID_TESTER_RECORDER.mp4"
+//        )
+//    }
 
 }
 
